887771b969e5917f5c4641a0257be0c088afd42e
James Devine <j.devine@lancaster.ac.uk>
2018-05-20 21:55:42 +0100

    Modifications to blocks package.

--- enums.d.ts
+++ enums.d.ts
@@ -1,4 +1,25 @@
 // Auto-generated. Do not edit.
+
+
+    declare const enum EnergyLevel {
+    under = 0,
+    over = 1,
+    }
+
+
+    declare const enum CarbonLevel {
+    high = 0,
+    low = 1,
+    }
+
+
+    declare const enum Locations {
+    school = 0,
+    home = 1,
+    classroom = 2,
+    microbitfoundation = 3,
+    everyone = 4,
+    }
 declare namespace weather {
 }
 
--- weather.cpp
+++ weather.cpp
@@ -1,29 +1,64 @@
 #include "pxt.h"
 #include "WeatherService.h"
 
-WeatherService s(uBit.radio);
+WeatherService weatherService(uBit.radio);
 
-void log_string(const char *)
+void log_string(const char *s)
 {
-
+    printf("%s",s);
 }
 
 void log_num(int num)
 {
-
+    printf("%d",num);
 }
 
-//% color=190 weight=100 icon="\uf1ec" block="IoT Blocks"
+enum class EnergyLevel{
+    under = 0,
+    over = 1
+};
+
+enum class CarbonLevel{
+    high = 0,
+    low = 1
+};
+
+enum class Locations{
+    school = 0,
+    home = 1,
+    classroom = 2,
+    microbitfoundation = 3,
+    everyone = 4,
+};
+
+/**
+ * share
+ * use
+ */
+
+//% color=243 weight=100 icon="\uf1ec" block="IoT Blocks"
 namespace weather {
     /**
       * do some stuff
       */
     //% help=none
     //% weight=96
-    //% blockId=set_room_temp block="set|room %room|temperature to %temperature" blockGap=8
-    void setRoomTemperature(StringData* room, int temperature)
+    //% blockId=set_room_temp block="send|temperature %temperature|of room %room to remote" blockGap=8
+    void setRoomTemperature(int temperature, StringData* room)
     {
-        w.setRoomTemperature(ManagedString(room),temperature)
+        weatherService.setRoomTemperature(ManagedString(room), temperature);
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=share block="share|%variable|with %PERMISSIONS" blockGap=8
+    //% PERMISSIONS.fieldEditor="gridpicker" PERMISSIONS.fieldOptions.columns=4
+    void share(StringData* variable, Locations l)
+    {
+        // w.setRoomTemperature(ManagedString(room),temperature)
     }
 
     /**
@@ -34,7 +69,75 @@ namespace weather {
     //% blockId=get_place_temp block="get|temperature for %location" blockGap=8
     StringData* getTemperature(StringData* location)
     {
-        ManagedString s = w.getTemperature(ManagedString(location),temperature);
+        ManagedString s = weatherService.getTemperature(ManagedString(location));
         return s.leakData();
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=get_energy block="get|energy usage of building %building" blockGap=8
+    StringData* getEnergyUsage(StringData* building)
+    {
+        return ManagedString().leakData();
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=on_energy_event block="on energy usage of building %building|%LEVEL|%threshold" blockGap=8
+    //% LEVEL.fieldEditor="gridpicker" LEVEL.fieldOptions.columns=4
+    void onEnergy(StringData* building, EnergyLevel level, int threshold, Action body)
+    {
+        //
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=on_carbon_event block="on|carbon level %LEVEL" blockGap=8
+    //% LEVEL.fieldEditor="gridpicker" LEVEL.fieldOptions.columns=4
+    void onCarbon(CarbonLevel level, Action body)
+    {
+        //
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=on_any block="on %key|in room %room| changed" blockGap=8
+    void onAny(StringData* key, StringData* room, Action body)
+    {
+        //
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=get_received_string block="received string" blockGap=8
+    StringData* getReceivedString()
+    {
+        return ManagedString().leakData();
+    }
+
+    /**
+      * do some stuff
+      */
+    //% help=none
+    //% weight=96
+    //% blockId=get_received_number block="received number" blockGap=8
+    int getReceivedNumber()
+    {
+        return 100;
     }
 }
